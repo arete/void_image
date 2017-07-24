4f26ff97fd54f97de312c60844846c8fdf73847f
Gerardo Di Iorio <arete74@gmail.com>
2017-07-24 08:38:45 +0200

    cache update pkg
    Signed-off-by: Gerardo Di Iorio <arete74@gmail.com>

--- mirror.sh
+++ mirror.sh
@@ -1,4 +1,7 @@
-xbps-install -Syu -R https://repo.voidlinux.eu/current/ -r /root/void/root -c /root/void/cache base-system etckeeper grub 
+mkdir -p cache root
+
+
+xbps-install -Syu -R https://repo.voidlinux.eu/current/ -r /root/void/root -c /root/void/cache base-system etckeeper grub  samba postgresql postgresql-client php-pgsql nginx php php-fpm
 
 
 cd cache
