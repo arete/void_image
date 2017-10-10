e630e1fafacccdb8008555b63724058c6c5e8918
Gerardo Di Iorio <arete74@gmail.com>
2017-10-10 07:11:57 +0200

    full sync
    Signed-off-by: Gerardo Di Iorio <arete74@gmail.com>

--- mirror.sh
+++ mirror.sh
@@ -1,7 +1,7 @@
 mkdir -p cache root
 
 
-xbps-install -Syu -R https://repo.voidlinux.eu/current/ -r /root/void/root -c /root/void/cache base-system etckeeper grub  samba dovecot dovecot-plugin-pigeonhole dovecot-plugin-lucene dovecot-plugin-sqlite fetchmail openldap postgresql postgresql-client php-pgsql nginx php php-fpm
+xbps-install -Syu -R https://repo.voidlinux.eu/current/ -r /root/void/root -c /root/void/cache base-system etckeeper grub cronie  samba dovecot dovecot-plugin-pigeonhole dovecot-plugin-lucene dovecot-plugin-sqlite fetchmail openldap postgresql postgresql-client php-pgsql nginx php php-fpm
 
 
 cd cache
